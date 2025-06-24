f5acfb95cd8c998e135f8acb0b139f771ff0845e
Joscelys22 <Jrojas.9295@unimar.edu.ve>
2025-06-24 17:19:55 -0400

    conexion a la base de datos

--- src/database/db.php
+++ src/database/db.php
@@ -7,7 +7,7 @@ $dbname = "bioderma";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+    die("Conexion fallida: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
